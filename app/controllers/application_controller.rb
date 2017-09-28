b6f3bc40bac8e2229d4d043ff374e35096a1e16c
Tejas <tejas.tholpadi7@gmail.com>
2017-09-28 20:15:38 +0530

    theme

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if (devise_controller? && resource_name == :user && action_name != 'edit') ||  
+      (controller_path.eql?('devise/passwords') && resource_name == :user)
+      "homepages"
+    elsif controller_name == 'home' && action_name == 'index'
+      "landing"
+    else
+      "application"
+    end
+  end
+
+  def check_guest
+    if user_signed_in?
+      redirect_to products_path
+    end
+  end
+
   def after_sign_in_path_for(resource)
     # if resource.admin?
     #   rails_admin_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root 'products#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'home#index', as: :unauthenticated_root
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products, only: [:index]
   resources :orders, only: [:create, :index]
@@ -9,5 +18,5 @@ Rails.application.routes.draw do
   post '/product/rate' => 'products#rate'
   get '/paypal/checkout', to: 'orders#paypal_checkout'
   get '/paypal/complete', to: 'orders#complete_payment'
-  root "home#index"
+  # root "home#index"
 end
